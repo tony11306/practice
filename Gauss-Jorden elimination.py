fe0c18cb9918adffbddb9cce5440a24bcb20121e
Tony11306 <50052865+tony11306@users.noreply.github.com>
2020-12-06 19:44:27 +0800

    Update Gauss-Jorden elimination.py

--- Gauss-Jorden elimination.py
+++ Gauss-Jorden elimination.py
@@ -1,130 +1,35 @@
-def solve(matrix):
-
-    ans = []
-    n = len(matrix)
-    # ------------------------------------------------------
-    '''
-    This part will eliminate lower triangular
-    '''
-    for i in range(n):
-        if matrix[i][i] == 0:
-            for rowIndex in range(i+1, len(matrix)):
-                if matrix[rowIndex][i] != 0:
-                    matrix = rowSwap(matrix, i, rowIndex)
-                    break
-
-        for j in range(i, n):
-          
-            if matrix[j][i] == 0:
-                continue
-            lcmNum = lcm(matrix[i][i], matrix[j][i])
-            matrix = rowMultiply(matrix, j, lcmNum // matrix[j][i])
-            matrix = rowMultiply(matrix, i, -lcmNum // matrix[i][i])
-            matrix = rowAdd(matrix, j, i)
-    # ------------------------------------------------------  
-    if matrix[n-1][n] == 0 and matrix[n-1][n-1] == 0: # if last row become 0 0 0 .... 0 0, then it has infinite solutions.
-        printMatrix(matrix)
-        print('無限多組解')
-        return
-    elif matrix[n-1][n] != 0 and matrix[n-1][n-1] == 0: # if last row become 0 0 0 ... 0 k, where k != 0, then it has no solution.
-        printMatrix(matrix)
-        print('無解')
-        return
-    # -------------------------------------------------------
-    '''
-    This part will eliminate the upper triangular
-    '''
-    for i in range(n-1, -1, -1):
-        for j in range(i-1, -1, -1):
-            if matrix[j][i] == 0:
-                continue
-            lcmNum = lcm(matrix[i][i], matrix[j][i])
-            matrix = rowMultiply(matrix, j, lcmNum // matrix[j][i])
-            matrix = rowMultiply(matrix, i, -lcmNum // matrix[i][i])
-            matrix = rowAdd(matrix, j, i)
-    printMatrix(matrix)
-    for i in range(n):
-        ans.append(matrix[i][n] / matrix[i][i])
-    return ans
-        
-def lcm(a: int, b: int):
-    '''
-    This function returns the least common multiple
-    '''
-    if a == 0 or b == 0:
-        return 0
-    return (a * b) // gcd(a, b)
-
-def gcd(a: int, b: int):
-    '''
-    This function returns the greatest common divisor of a and b
-    '''
-    if a == 0 or b == 0:
-        return 0
-
-    if a > b: # make sure that a is smaller than b, since we're gonna do swap first and then divide in the while loop
-        temp = a
-        a = b
-        b = temp
-    while a != 0:
-        # swap a and b
-        temp = a
-        a = b
-        b = temp
-
-        a %= b
-        
-    return b
-
-def printMatrix(matrix):
-    print('')
-    for i in matrix:
-        for j in i:
-            print(j, end=' ')
-        print('')
-
-def rowMultiply(matrix, rowIndex, k):
-    '''
-    This function will return the matrix where its rowIndex'th row is multipied by k.
-    '''
-    for i in range(len(matrix[rowIndex])):
-        matrix[rowIndex][i] *= k
-
-    return matrix
-
-def rowAdd(matrix, firstRowIndex, secondRowIndex):
-    '''
-    This function will add the second row to the first row, and then return the matrix.
-    '''
-    for i in range(len(matrix[firstRowIndex])):
-        matrix[firstRowIndex][i] += matrix[secondRowIndex][i]
+# 此程式沒有對無解和無限多組解作處理
+def gaussElimination(matrix, n: int):
     
+    def swapRow(a, b):
+        matrix[a], matrix[b] = matrix[b], matrix[a]
+    
+    def removeNegativeZero():
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if abs(matrix[i][j]) < 0.000000001:
+                    matrix[i][j] = 0.0
+    
+    for i in range(n): # 控制橫向
+        maxIndex = i
+        for j in range(i, n): # 控制縱向
+            if abs(matrix[maxIndex][i]) < abs(matrix[j][i]): # 找出絕對值最大值，移到上層，用意是為了不要出現領導係數為0的狀況
+                maxIndex = j
+        swapRow(maxIndex, i)
+        val = matrix[i][i]
+        for j in range(len(matrix[i])): # 把領導係數變成1
+            matrix[i][j] /= val
+        for j in range(i+1, n): # 開始對下面列做消去
+            val = matrix[j][i]
+            for k in range(i, len(matrix[j])):
+                matrix[j][k] += matrix[i][k]*(-val)
+                
+
+    for i in range(n-2, -1, -1): # 處理上三角
+        for j in range(i, -1, -1):
+            val = matrix[j][i+1]
+            for k in range(i, len(matrix[i])): # 往上做消去
+                matrix[j][k] += matrix[i+1][k]*(-val)
+                
+    removeNegativeZero()
     return matrix
-
-def rowSwap(matrix, firstRowIndex, secondRowIndex):
-    '''
-    This function will swap matrix[firstRowIndex] and matrix[secondRowIndex].
-    '''
-    for i in range(len(matrix[firstRowIndex])):
-        temp = matrix[firstRowIndex][i]
-        matrix[firstRowIndex][i] = matrix[secondRowIndex][i]
-        matrix[secondRowIndex][i] = temp
-    return matrix
-
-# main
-
-print(f'n元一次聯立方程式?')
-n = int(input())
-print(f'請每列輸入{n+1}個元素，共{n}列')
-
-matrix = []
-for i in range(n):
-    matrix.append([])
-    rows = input().split()
-    for row in rows:
-        matrix[i].append(int(row))
-
-ans = solve(matrix)
-print(ans)
-        
-
